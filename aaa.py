1adaeabd8b608d14a716c32e55a56b04730cb05a
ammin <annareshnaidu143@gmail.com>
2023-01-12 19:24:33 +0530

    python2

---
+++ aaa.py
@@ -0,0 +1,14 @@
+number1=60
+number2=740
+add=a+b
+print("sum of two values")
+
+sub=b-a
+print("the multiplication of two values are ",sub)
+
+div =b/a
+print ("the division of two values,mod")
+
+floor=b//a
+print("fioor value of two values",floor)
+
--- naidu.py
+++ naidu.py
@@ -3,8 +3,5 @@ b=input ('Enter B value')
 sum= float(a) + float(b)
 print('The sum of Given values {0} and {1} ={2}'.fromat(a, b, sum))
 
-a=100
-print ("the given variable value is",a"is,type(a))
-b=20.22
-print ("the given variable value is",bis,type(b))
-c=300.33("the given varibles value is",c"type(c))
+a=60
+b=40
